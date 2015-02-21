b276a48e66169184cd47a19cc94f329a92f22b4c
Debjit Pal <dpal2@illinois.edu>
2015-02-21 12:48:40 -0800

    First Commit on my branch

--- mp2.c
+++ mp2.c
@@ -2,3 +2,16 @@
 //	Author: Debjit Pal			//
 //	Email: dpal2@illinois.edu		//
 //////////////////////////////////////////////////
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/timer.h>
+#include <linux/proc_fs.h>
+#include <linux/list.h>
+#include <linux/uaccess.h>
+#include <linux/sched.h>
+#include <linux/kthread.h>
+#include <linux/mutex.h>
+#include <linux/sysfs.h>
+#include <linux/jiffies.h>
+#include "mp2_given.h"
